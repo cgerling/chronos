9431bdb216e54d9248740fa42abfdf4e193b9125
Calvin Gerling <calvingerling@outlook.com>
2017-12-26 21:46:51 -0200

    feat(Icon): add generic icon component
    isolate the icon specific markup to be interchangeable

---
+++ src/components/Icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <i class="material-icons">{{name}}</i>
+</template>
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
