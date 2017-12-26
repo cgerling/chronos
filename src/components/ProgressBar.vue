5a6960b4232dea68d4b540dea312a752871f90c9
Calvin Gerling <calvingerling@outlook.com>
2017-12-26 21:45:38 -0200

    feat(ProgressBar): add custom progress bar component
    create a custom progress bar to use in the category card

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="progress-bar" :style="{ background: color }">
+    <div class="mask" :style="{ width: width }"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProgressBar',
+  props: {
+    percentage: {
+      type: Number,
+      default: 0
+    },
+    color: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    asPercent (number) {
+      return `${number}%`
+    }
+  },
+  computed: {
+    width () {
+      const widthPercent = 100 - this.percentage
+      return this.asPercent(widthPercent)
+    }
+  }
+}
+</script>
+<style scoped>
+.progress-bar {
+  border-radius: 5px 5px 0 0;
+  height: 100%;
+  position: relative;
+  width: 100%;
+}
+
+.progress-bar::before {
+  background: linear-gradient(90deg, transparent, rgba(0, 0, 0, .5));
+  content: '';
+  display: block;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+}
+
+.progress-bar::after {
+  background: #fff;
+  bottom: 0;
+  border-radius: 5px 5px 0 0;
+  content: '';
+  display: block;
+  height: 50%;
+  position: absolute;
+  width: 100%;
+  z-index: 10;
+}
+
+.mask {
+  background: rgba(255, 255, 255, .75);
+  content: '';
+  display: block;
+  height: 100%;
+  position: absolute;
+  right: 0;
+  width: 100%;
+  z-index: 2;
+}
+</style>
