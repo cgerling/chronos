f6854b493dbb6431386d33b19f8473d4cd20b17d
Calvin Gerling <calvingerling@outlook.com>
2017-12-26 21:49:29 -0200

    feat(Home): add the Home container
    add a container of home page with a card as example

---
+++ src/containers/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <category-card name="Work" color="#0F81C7" />
+  </div>
+</template>
+<script>
+import CategoryCard from '@/components/CategoryCard'
+
+export default {
+  name: 'Home',
+  components: { CategoryCard }
+}
+</script>
+<style scoped>
+</style>
