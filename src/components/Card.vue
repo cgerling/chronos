6ab24fbfa30031991c0f98330545a28e5ea29fec
Calvin Gerling <calvingerling@outlook.com>
2017-12-26 22:01:07 -0200

    feat(Card): add card component
    add a generic card container

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+<style scoped>
+.card {
+  background: #fff;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(100, 100, 100, .5);
+  box-sizing: border-box;
+}
+</style>
