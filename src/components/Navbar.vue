9aa5d17f0bc06bd46fcd780ca190ea2b8cde1fe5
Calvin Gerling <calvingerling@outlook.com>
2017-12-29 21:55:54 -0200

    feat(Navbar): add component
    Create component to show the page title, cancel the operation and go back to previous page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="navbar">
+    <div class="icon left" @click="back()">
+      <icon name="arrow_back" />
+    </div>
+    <p class="title">
+      <slot />
+    </p>
+    <div class="icon right" @click="cancel()">
+      <icon name="close" />
+    </div>
+  </nav>
+</template>
+<script>
+import Icon from '@/components/Icon'
+
+export default {
+  name: 'Navbar',
+  components: { Icon },
+  methods: {
+    back () {
+      this.$router.go(-1)
+    },
+    cancel () {
+      this.$router.replace('/')
+    }
+  }
+}
+</script>
+<style scoped>
+.navbar {
+  align-items: center;
+  display: flex;
+  height: 56px;
+  justify-content: space-between;
+}
+
+.icon {
+  padding: 16px;
+  pointer-events: none;
+  user-select: none;
+  visibility: hidden;
+}
+
+.title {
+  margin: 0;
+}
+
+nav[back] .left,
+nav[close] .right {
+  pointer-events: auto;
+  visibility: visible;
+}
+</style>
