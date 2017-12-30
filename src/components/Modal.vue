d227094f6dd5f0b4450179774fd41f02cf41d108
Calvin Gerling <calvingerling@outlook.com>
2017-12-30 14:38:23 -0200

    feat(Modal): add generic Modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,61 @@
+<template>
+  <div v-if="show" class="modal">
+    <div class="head">
+      <p class="title">
+        {{title}}
+      </p>
+      <div @click="close()">
+        <icon name="close" />
+      </div>
+    </div>
+    <div class="body">
+      <slot />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    show: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style scoped>
+.modal {
+  background: #fff;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 100;
+}
+
+.modal .head {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: .25em .5em;
+}
+
+.modal .title {
+  margin: 0;
+}
+
+.modal .body {
+  padding: .5em;
+}
+</style>
