5c1d9bfc2c3dfa98b8d51a56bfbbe9c9715ace62
Calvin Gerling <calvingerling@outlook.com>
2017-12-29 22:42:22 -0200

    feat(ColorPicker): add custom color picker component

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,57 @@
+<template>
+  <card class="color-picker">
+    <div class="color" @click="select(color)" v-for="color in colors" :key="color" :style="{ background: color }">
+      <icon name="check_circle" v-if="color == selected" />
+    </div>
+  </card>
+</template>
+<script>
+export default {
+  name: 'ColorPicker',
+  props: {
+    colors: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      selected: ''
+    }
+  },
+  methods: {
+    select (color) {
+      this.selected = color
+
+      this.$emit('selected', color)
+    }
+  }
+}
+</script>
+<style scoped>
+.color-picker {
+  border-radius: 5px;
+  display: flex;
+  height: 50px;
+  margin: 1em 0;
+  width: 100%;
+}
+
+.color {
+  align-items: center;
+  color: #fff;
+  display: inline-flex;
+  justify-content: center;
+  width: inherit;
+}
+
+.color:first-of-type {
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+}
+
+.color:last-of-type {
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+</style>
