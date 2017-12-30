4f2f6e094e25660026c7bcb9e9f56f8e5cc65eda
Calvin Gerling <calvingerling@outlook.com>
2017-12-29 22:42:57 -0200

    feat(SubmitButton): add button to confirm operations

---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button :style="{ background: color }">
+    <div class="content">
+      <slot />
+    </div>
+  </button>
+</template>
+<script>
+export default {
+  name: 'SubmitButton',
+  props: {
+    color: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+button {
+  border: 0;
+  color: #fff;
+  height: 56px;
+  padding: 0;
+  position: relative;
+  width: 100%;
+}
+
+button::before {
+  background: linear-gradient(90deg, transparent, rgba(0, 0, 0, .5));
+  content: '';
+  display: block;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+
+button .content {
+  color: inherit;
+  position: relative;
+  z-index: 10;
+}
+</style>
