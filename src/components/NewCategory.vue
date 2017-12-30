40fd0be97a873a9e3e4da0ebde7e29c184c033c3
Calvin Gerling <calvingerling@outlook.com>
2017-12-30 21:27:26 -0200

    refactor(NewCategory): put the icon side-by-side with the name

--- src/components/NewCategory.vue
+++ src/components/NewCategory.vue
@@ -1,7 +1,9 @@
 <template>
   <card class="new-category">
-    <icon-picker @select="selectIcon" />
-    <input class="field name" type="text" placeholder="Name" v-model="name" @blur="update()">
+    <div class="main-info">
+      <icon-picker @select="selectIcon" />
+      <input class="field name" type="text" placeholder="Name" v-model="name" @blur="update()">
+    </div>
     <textarea class="field description" placeholder="Description (optional)" v-model="description" @blur="update()" />
   </card>
 </template>
@@ -42,9 +44,20 @@ export default {
   padding: 5px;
 }
 
+.new-category .main-info {
+  align-items: center;
+  display: flex;
+}
+
+.new-category .main-info .name {
+  text-indent: 0;
+  padding-left: .5em;
+}
+
 .field {
   border: none;
   display: block;
+  flex-grow: 1;
   font-family: inherit;
   font-size: 1em;
   margin: 5px 0;
@@ -53,7 +66,6 @@ export default {
 }
 
 .description {
-  flex: 1;
   padding: 0;
 }
 
