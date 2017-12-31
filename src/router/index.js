4c5c4cd269594c7bdbcf1a21ab840f8e52a9ea1e
Calvin Gerling <calvingerling@outlook.com>
2017-12-31 14:50:22 -0200

    feat(Router): map a route to create a category

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from '@/containers/Home'
+import CreateCategory from '@/containers/CreateCategory'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/category/new',
+      name: 'Create Category',
+      component: CreateCategory
     }
   ]
 })
