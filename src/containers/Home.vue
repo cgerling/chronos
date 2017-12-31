b7f0cd21eaa68f48184541e92dccb8c927200acf
Calvin Gerling <calvingerling@outlook.com>
2017-12-31 14:49:15 -0200

    feat(Home): list all the categories in page

--- src/containers/Home.vue
+++ src/containers/Home.vue
@@ -1,14 +1,32 @@
 <template>
   <main class="container">
-    <category-card name="Work" color="#0F81C7" />
+    <category-card v-for="category in categories" :key="category.name" :name="category.name" :color="category.color" :icon="category.icon" />
   </main>
 </template>
 <script>
 import CategoryCard from '@/components/CategoryCard'
+import Database from '@/services/indexeddb'
 
 export default {
   name: 'Home',
-  components: { CategoryCard }
+  components: { CategoryCard },
+  data () {
+    return {
+      categories: []
+    }
+  },
+  methods: {
+    getCategories () {
+      return Database.connect('chronos')
+        .then(db => Database.findAll(db, 'category'))
+        .catch(console.error.bind(console))
+    }
+  },
+  created () {
+    this.getCategories().then(categories => {
+      this.categories = categories
+    })
+  }
 }
 </script>
 <style scoped>
