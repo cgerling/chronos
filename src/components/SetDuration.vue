7a776ffa853b61fa0dfcdc628e3d04aca7dc6e3d
Calvin Gerling <calvingerling@outlook.com>
2018-01-03 23:58:12 -0200

    feat: add components SetReminder and SetDuration
    add components of future funcionalities

---
+++ src/components/SetDuration.vue
@@ -0,0 +1,20 @@
+<template>
+  <card class="set-duration">
+    <icon name="access_time" />
+    Duration
+  </card>
+</template>
+<script>
+export default {
+  name: 'SetDuration'
+}
+</script>
+<style scoped>
+.set-duration {
+  align-items: center;
+  display: flex;
+  height: 50px;
+  justify-content: space-around;
+  padding: .5em;
+}
+</style>
---
+++ src/components/SetReminder.vue
@@ -0,0 +1,20 @@
+<template>
+  <card class="set-reminder">
+    <icon name="add_alert" />
+    Reminder
+  </card>
+</template>
+<script>
+export default {
+  name: 'SetReminder'
+}
+</script>
+<style scoped>
+.set-reminder {
+  align-items: center;
+  display: flex;
+  height: 50px;
+  justify-content: space-around;
+  padding: .5em;
+}
+</style>
