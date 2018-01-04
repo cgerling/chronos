e6270e1f9cd7f358c675b765f02905cf01825340
Calvin Gerling <calvingerling@outlook.com>
2018-01-03 23:57:26 -0200

    feat(CategoryPicker): show info about the category
    Show info about a category selected, is planned to allow edit change the selected category

---
+++ src/components/CategoryPicker.vue
@@ -0,0 +1,53 @@
+<template>
+  <card class="category">
+    <icon name="copyright" />
+    <p class="name">
+      {{category}}
+    </p>
+  </card>
+</template>
+<script>
+import Card from '@/components/Card'
+import categoryService from '@/services/categoryService'
+
+export default {
+  name: 'CategoryPicker',
+  components: { Card },
+  props: {
+    category: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      selected: {
+        icon: '',
+        name: ''
+      },
+      categories: []
+    }
+  },
+  created () {
+    categoryService.getAll().then(categories => {
+      this.categories = categories.map(category => ({
+        icon: category.icon,
+        name: category.name
+      }))
+    })
+  }
+}
+</script>
+<style scoped>
+.category {
+  align-items: center;
+  display: flex;
+  padding: .5em;
+}
+
+.category .name {
+  margin: 0;
+  margin-left: .5em;
+  flex-grow: 1;
+}
+</style>
