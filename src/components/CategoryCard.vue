3bb1893fde04aa28678ba34dea38173398102c40
Calvin Gerling <calvingerling@outlook.com>
2018-01-07 15:31:03 -0200

    feat(CategoryCard): add the Percentage and TaskCounter to the card

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,7 +1,12 @@
 <template>
   <card class="category-card">
     <progress-bar :color="category.color" />
+    <router-link class="content" :to="`/category/${category.name}`" tag="div">
+      <div class="info">
         <icon :name="category.icon" />
+        <percentage :fraction="tasksDone.length" :total="category.tasks.length" />
+      </div>
+      <task-counter class="task-counter" :total="category.tasks.length" />
       <p class="title">{{category.name}}</p>
     </router-link>
     <router-link class="new-task" tag="div" :to="`/task/new?category=${category.name}`">
@@ -12,6 +17,8 @@
 </template>
 <script>
 import ProgressBar from '@/components/ProgressBar'
+import Percentage from '@/components/Percentage'
+import TaskCounter from '@/components/TaskCounter'
 
 export default {
   name: 'CategoryCard',
@@ -26,7 +33,12 @@ export default {
       }
     }
   },
-  components: { ProgressBar }
+  components: { ProgressBar, Percentage, TaskCounter },
+  computed: {
+    tasksDone () {
+      return this.category.tasks.filter(category => category.completed)
+    }
+  }
 }
 </script>
 <style scoped>
@@ -40,11 +52,19 @@ export default {
 }
 
 .content {
-  align-items: center;
+  align-items: flex-start;
   box-shadow: 0 2px 10px rgba(100, 100, 100, .5);
   display: flex;
   flex-direction: column;
   justify-content: center;
+  padding: 0 .5em .5em;
+}
+
+.content > .info {
+  align-items: center;
+  align-self: stretch;
+  display: flex;
+  justify-content: space-between;
 }
 
 .title {
@@ -59,5 +79,9 @@ export default {
   align-items: center;
   display: flex;
   justify-content: center;
+}
+
+.task-counter {
+  align-self: center;
 }
 </style>
