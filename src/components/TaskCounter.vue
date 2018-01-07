5288965fc65d72ba281f4ce0b1378fa353665e95
Calvin Gerling <calvingerling@outlook.com>
2018-01-07 15:29:15 -0200

    feat(TaskCounter): create component to properly exhibit the remaining tasks

---
+++ src/components/TaskCounter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="task-counter">
+    <h2>{{total}}</h2>
+    <p class="text">tasks</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TaskCounter',
+  props: {
+    total: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+.task-counter {
+  align-items: baseline;
+  display: flex;
+  justify-content: center;
+}
+
+.task-counter > .text {
+  margin-left: 5px;
+}
+</style>
