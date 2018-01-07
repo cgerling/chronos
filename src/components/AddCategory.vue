c60638f64eccd7e22a3283896a7526b370dcf56d
Calvin Gerling <calvingerling@outlook.com>
2018-01-07 15:23:32 -0200

    feat(AddCategory): create component to link page of category creation

---
+++ src/components/AddCategory.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link to="/category/new" tag="div">
+    <card class="add-category">
+      <icon name="add" />
+      Add category
+    </card>
+  </router-link>
+</template>
+<script>
+import Card from '@/components/Card'
+
+export default {
+  name: 'AddCategory',
+  components: { Card }
+}
+</script>
+<style scoped>
+.add-category {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+  justify-content: center;
+  min-height: 200px;
+}
+</style>
