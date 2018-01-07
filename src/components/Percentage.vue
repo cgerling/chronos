16687b93fefa6dbfe125bd2fff4b4909766579ff
Calvin Gerling <calvingerling@outlook.com>
2018-01-07 15:28:36 -0200

    feat(Percentage): crete component to show percentage as pure text

---
+++ src/components/Percentage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="percentage">
+    {{percentage}}
+  </div>
+</template>
+<script>
+import { percentageOf } from '@/services/percentService'
+
+export default {
+  name: 'Percentage',
+  props: {
+    total: {
+      type: Number,
+      required: true
+    },
+    fraction: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    percentage () {
+      return `${percentageOf(this.fraction, this.total)} %`
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
