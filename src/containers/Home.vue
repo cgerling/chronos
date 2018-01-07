db97cc7fbe0e62d59af67e51aff169b32baf7c3c
Calvin Gerling <calvingerling@outlook.com>
2018-01-07 15:24:17 -0200

    feat(Home): add the component link in home page

--- src/containers/Home.vue
+++ src/containers/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <main class="container">
     <category-card v-for="category in categories" :key="category.name" :name="category.name" :color="category.color" :icon="category.icon" />
+    <add-category />
   </main>
 </template>
 <script>
 import CategoryCard from '@/components/CategoryCard'
+import AddCategory from '@/components/AddCategory'
 import categoryService from '@/services/categoryService'
 
 export default {
   name: 'Home',
-  components: { CategoryCard },
+  components: { CategoryCard, AddCategory },
   data () {
     return {
       categories: []
